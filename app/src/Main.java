790b2aef64194b0613b683313951b60202b8f7d8
antoniovidov <91030382+antoniovidov@users.noreply.github.com>
2025-06-25 16:05:32 +0300

    Added Read and Write methods to the main

--- app/src/Main.java
+++ app/src/Main.java
@@ -1,8 +1,11 @@
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -21,8 +24,17 @@ public class Main {
         double fuel = sc.nextDouble();
         double fuelConsumption = (fuel / distance) * 100;
         fuelConsumption = Math.round((fuelConsumption * 100.0) / 100.0);
+        System.out.println(fuelConsumption);
+        String toBeWritten = fuelConsumption + " : " + date;
 
-
+        ArrayList<String> readData = readFile(data);
+        boolean flag = readData.isEmpty();
+        if(flag){
+            writeFile(data, toBeWritten, flag);
+        }else{
+            System.out.println(readData);
+            writeFile(data, toBeWritten, flag);
+        }
     }
 
     public static LocalDate inputDate(String dateString) {
@@ -30,7 +42,6 @@ public class Main {
         LocalDate date = null;
         try {
             date = LocalDate.parse(dateString, formatter);
-            System.out.println("Parsed date: " + date);
         } catch (DateTimeParseException e) {
             System.out.println("Invalid date format. Please use dd.MM.yyyy.");
         }
@@ -38,8 +49,9 @@ public class Main {
     }
 
     public static File createFile(){
+        File data = null;
         try{
-            File data = new File("fuel_consumption.txt");
+            data = new File("fuel_consumption.txt");
             if(data.createNewFile()){
                 System.out.println("File created!");
                 return data;
@@ -49,6 +61,39 @@ public class Main {
         }catch (IOException e){
             System.out.println(e.getMessage());
             e.printStackTrace();
+        }
+        return data;
+    }
+
+    public static ArrayList<String> readFile(File file){
+        ArrayList<String> done = new ArrayList<>();
+        try{
+            Scanner reader = new Scanner(file);
+            while(reader.hasNextLine()){
+                done.add(reader.nextLine());
+            }
+            reader.close();
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }catch(NullPointerException e){
+            System.out.println(e.getMessage());
+        }
+        return done;
+    }
+
+    public static void writeFile(File file, String message, boolean flag){
+        try{
+            BufferedWriter writer = new BufferedWriter( new FileWriter(file.getAbsolutePath(), true));
+            if(flag){
+                writer.write(message);
+            }else{
+                writer.append("\n");
+                writer.append(message);
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
         }
     }
 }
